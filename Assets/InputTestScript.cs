997bd6ede617a0cc0d3f63f1af1238cfc7400dcf
Bhargava Manja <Bhargee.Manja@gmail.com>
2015-04-18 14:53:20 -0500

    Starting integration work + keyboard input
    ^

---
+++ Assets/InputTestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using vrim;
+
+public class InputTestScript : MonoBehaviour {
+	Buffer b;
+	Canvas c;
+	// Use this for initialization
+	void Start () {
+		b = new Buffer ();
+		//GameObject go = GameObject.Find ("Canvas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			Text t = (GameObject.Find ("Text")).GetComponent<Text>();
+			b.Insert(Input.inputString);
+			t.text = b.GetContents();
+		}
+	}
+}
