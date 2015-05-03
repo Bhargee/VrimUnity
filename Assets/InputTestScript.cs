71f0e56d087319220bb726b15f7934238b162886
Ben Fishbein <fishbeinb@gmail.com>
2015-05-03 16:51:15 -0500

    gaFD

--- Assets/InputTestScript.cs
+++ Assets/InputTestScript.cs
@@ -13,35 +13,27 @@ public class InputTestScript : MonoBehaviour {
 	InputField i;
 	// Use this for initialization
 	void Start () {
-		b = new Buffer ();
-		t = (GameObject.Find ("Text")).GetComponent<Text>();
-		//StartCoroutine("ProcInput");
 		c = GameObject.Find ("Canvas");
+		
 		Vector3 newPos = new Vector3(Sphere_Size * Mathf.Cos (0) * Mathf.Sin(0), Sphere_Size * Mathf.Sin (0) * Mathf.Sin (0), Sphere_Size * Mathf.Cos (0));
-		c.transform.position = newPos;
-		int x, y, z;
-		float r;
-		x = 200;
-		y = 200;
-		z = 200;
-		r = Mathf.Sqrt (x * x + y * y + z * z);
-		c2 = GameObject.Find ("Canvas2");
-		Vector3 newPos2 = new Vector3(r * Mathf.Cos (Mathf.Atan(x/y)) * Mathf.Sin(Mathf.Acos(z/r)), r * Mathf.Sin (Mathf.Atan(x/y)) * Mathf.Sin (Mathf.Acos(z/r)), r * Mathf.Cos (Mathf.Acos(z/r)));
-		c2.transform.position = newPos2;
-		c2.transform.Rotate (x, y, z);
-		i = (GameObject.Find ("InputField")).GetComponent<InputField>();
-		//i.Select ();
-		//i.ActivateInputField();
+		GameObject new_1 = (GameObject) Instantiate(c, newPos, Quaternion.identity);
+		new_1.transform.position = newPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		StartCoroutine ("ProcInput");
+		if (Input.GetKeyDown(KeyCode.H)) {//Overwrite onselect highlight
+			Vector3 newPos = new Vector3(-1 * Sphere_Size * Mathf.Cos (0) * Mathf.Sin(0), -1 * Sphere_Size * Mathf.Sin (0) * Mathf.Sin (0), -1 * Sphere_Size * Mathf.Cos (0));
+			GameObject new_1 = (GameObject) Instantiate(c, newPos, Quaternion.identity);
+			new_1.transform.position = newPos;
+		}
+		//StartCoroutine ("ProcInput");
 	}
 
 	IEnumerator ProcInput() 
 	{
 				while (true) {
+			 
 						bool command = false;
 						if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)) {
 								command = true;
@@ -58,17 +50,14 @@ public class InputTestScript : MonoBehaviour {
 										Sphere_Size = Sphere_Size + .3F;
 										Vector3 newPos = new Vector3 (Sphere_Size * Mathf.Cos (0) * Mathf.Sin (0), Sphere_Size * Mathf.Sin (0) * Mathf.Sin (0), Sphere_Size * Mathf.Cos (0));
 										c.transform.position = newPos;
-				} else if (Input.GetKey (KeyCode.L)) {//Overwrite onselect highlight
-					i = (GameObject.Find ("InputField")).GetComponent<InputField>();
-					i.Select ();
-					i.ActivateInputField();
-				}else if (Input.GetKey (KeyCode.K)) {//Overwrite onselect highlight
-					i = (GameObject.Find ("InputField2")).GetComponent<InputField>();
-					i.Select ();
-					i.ActivateInputField();
-				}
+				} /*
+				else if (Input.GetKeyDown(KeyCode.H)) {//Overwrite onselect highlight
+					Vector3 newPos = new Vector3(-1 * Sphere_Size * Mathf.Cos (0) * Mathf.Sin(0), -1 * Sphere_Size * Mathf.Sin (0) * Mathf.Sin (0), -1 * Sphere_Size * Mathf.Cos (0));
+					GameObject new_1 = (GameObject) Instantiate(c, newPos, Quaternion.identity);
+					new_1.transform.position = newPos;
+				}*/
 						}
-						yield return new WaitForSeconds (.08f);
+						yield return new WaitForSeconds (1f);
 				}
 		}
 }
